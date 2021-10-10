7de60ec3337ea62495a38a19bc30ce5ad0bbae8c
WaterpigCZ <wtryoutube@seznam.cz>
2021-10-10 09:42:57 +0200

    Added the files

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 0;
+name = "OverthrowPlus Workshop Fix";
+timestamp = 5249149173913622868;
---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "Overthrow Waterpig"; //This name will be displayed only in game , specifically the expansion menu
+picture = "logo_overthrow.paa"; //Location of your addon's icon
+logo = "logo_overthrow.paa";
+logoOver = "logo_overthrow.paa";
+actionName = "Website";   //The name of the action
+action = "http://armaoverthrow.com/"; //The action , "Authors Website Link"
+description = "Overthrow - Waterpig"; // Addon Description
+overview = "A Revolution Simulator for Arma 3";
+author = "ARMAzac";
+overviewPicture = "logo_overthrow.paa";
